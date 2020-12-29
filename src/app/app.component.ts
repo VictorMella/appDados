f5a7c9c69eb357c83f85a214fb48d4b668575e2d
Victor <v.mellaq@gmail.com>
2020-12-29 09:02:06 -0300

    Se da funcionalidad a app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appDados';
+  dadoIzq = '../assets/img/dice1.png'
+  dadoDer = '../assets/img/dice4.png'
+  numero1: number;
+  numero2: number;
+
+
+
+  tirarDados(){
+    this.numero1 = Math.round( Math.random() * 5) + 1
+    this.numero2 = Math.round( Math.random() * 5) + 1
+    this.dadoIzq = `../assets/img/dice${this.numero1}.png`
+    this.dadoDer = `../assets/img/dice${this.numero2}.png`
+  }
+
+
 }
+
